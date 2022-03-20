d5d4b53f487791e8f1efbd3efa0dd9dbf231e99f
rigzba21 <jonathan.velando01@gmail.com>
2022-03-20 10:54:00 -0400

    testing out client call with current kubeconfig context

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+//use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use kube_client::{Client};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    //create  cluster client using the default context or .kube/config
+    // or the $KUBECONFIG environment variable
+    let _client = Client::try_default().await?;
+    let _apiserver_version = _client.apiserver_version().await.unwrap();
+    println!("{:#?}", _apiserver_version);
+
+    Ok(())
 }
